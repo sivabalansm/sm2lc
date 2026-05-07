f67904cd253a5907f03ba26d8adfd1ea1da54c79
Sivabalan Muthurajan <sivabalan.muthurajan@botpress.com>
2026-05-06 23:04:42 -0400

    Fix issue with pointer moving

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod item_list;
 mod item;
 mod db;
 use axum::{Router, routing::get, routing::post, response::Json};
-use serde_json::json;
+use std::sync::Arc;
 use crate::{db::DB, item::Item};
 
 const PORT : u64 = 3030;
@@ -17,14 +17,14 @@ async fn handler_get_all_items(db : &DB) -> Json<Vec<Item>>{
 
 #[tokio::main]
 async fn main() {
-    let db = DB::new();
+    let db = Arc::new(DB::new());
     let items = db.get_all_items().await.unwrap();
 
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/update_item", post(handler_update_item))
         .route("/put_item", post(handler_put_item))
-        .route("/get_all_items", get(|| async { handler_get_all_items(&db) }));
+        .route("/get_all_items", get(async move || handler_get_all_items(&(db.clone())).await ));
 
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{PORT}")).await.unwrap();
